0d9aedf2ad402f8de02734bf82876c91bade5a7d
KaramveerSinghSidhu <65443742+KaramveerSinghSidhu@users.noreply.github.com>
2022-04-13 19:37:36 -0600

    new version

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
 const express = require('express')
+const { redirect } = require('express/lib/response')
 const uri = process.env.CONNECTION_TO_DATABASE
 const mongoose = require('mongoose')
 const app = express()
@@ -35,13 +36,16 @@ app.get('/', async (req, res) => {
     students = await Student.find({})
     var hasStudnets = false
 
+    //this is not the most effiecent way but it gets the job done because i want it to look cool
     if(students != null){
-        hasStudnets = true
+        students.forEach(student => {
+            hasStudnets = true
+        })
     }
-
     res.render('index.ejs', {hasStudnets: hasStudnets, students: students})
 })
 
+//added a user to DB
 app.get('/add/students', async (req, res) => {
     students = await Student.find({})
     var id = 1
@@ -55,7 +59,6 @@ app.get('/add/students', async (req, res) => {
 })
 
 app.post('/students', async (req, res) => {
-    console.log('working')
     var studnet = new Student({
         id: req.body.id,
         name: req.body.name,
@@ -70,4 +73,43 @@ app.post('/students', async (req, res) => {
     }
 })
 
-app.listen(process.env.PORT || 1000)
+//deletes user from db
+app.get('/delete/students', async (req, res) => {
+    students = await Student.find({})
+
+    res.render('remove.ejs', {students: students})
+})
+
+app.post('/remove/students', async (req, res) =>{
+    var a = req.body
+    var id = parseInt(a.id)
+    await Student.findOneAndDelete({id: id})
+
+    res.redirect("/")
+})
+
+//updates user on db
+app.get('/student/:id', async (req, res) => {
+    student = await Student.findById({_id: req.params.id})
+
+    res.render('edit.ejs', {student: student})
+})
+
+app.post('/update/student/:id', async (req, res) => {
+    var studnet = new Student({
+        id: req.body.id,
+        name: req.body.name,
+        number: req.body.number,
+        age: req.body.age
+    })
+    try{
+        student = await studnet.save()
+        await Student.findByIdAndDelete({_id: req.params.id})
+        res.redirect('/')
+    }catch{
+        res.redirect('/add/students')
+    }
+})
+
+
+app.listen(process.env.PORT || 2000)
